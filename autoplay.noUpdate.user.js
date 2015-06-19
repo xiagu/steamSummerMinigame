490f9974e54be7d52819b934842b6536919d7ade
Seb Patane <seb@libero.com.au>
2015-06-19 23:53:29 +1000

    5.0.10

--- autoplay.noUpdate.user.js
+++ autoplay.noUpdate.user.js
@@ -2,7 +2,7 @@
 // @name Ye Olde Megajump
 // @namespace https://github.com/YeOldeWH/MonsterMinigameWormholeWarp
 // @description A script that runs the Steam Monster Minigame for you.  Now with megajump.  Brought to you by the Ye Olde Wormhole Schemers and DannyDaemonic
-// @version 5.0.9
+// @version 5.0.10
 // @match *://steamcommunity.com/minigame/towerattack*
 // @match *://steamcommunity.com//minigame/towerattack*
 // @grant none
