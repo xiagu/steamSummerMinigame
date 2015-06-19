3c7f49d6770a772396447cbea46d468c9693fef9
Seb Patane <seb@libero.com.au>
2015-06-19 10:40:49 +1000

    fix dumb logic
    If the level isn't a boss level, the player will attack the best target
    If the level is 100 and the player is empty, they will move into the
    boss (best target) lane
    If the level is 100 and the player has a purpose, they will move into
    the lane where all the action is happening

--- autoPlay.user.js
+++ autoPlay.user.js
@@ -460,8 +460,8 @@ function MainLoop() {
 	if (!isAlreadyRunning) {
 		isAlreadyRunning = true;
 
-		if ((level % 100 == 0 && bHaveItem(ABILITIES.WORMHOLE))
-				|| likeNewOn100 ) {
+		if ((level % 100 == 0) &&
+				(bHaveItem(ABILITIES.WORMHOLE) || bHaveItem(ABILITIES.LIKE_NEW) )) {
 			// On a WH level, jump everyone with wormholes to lane 0, unless there is a boss there, in which case jump to lane 1.
 			var targetLane = 0;
 			// Check lane 0, enemy 0 to see if it's a boss
